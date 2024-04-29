90431fa715be3fd482bcb308b8a7f095838d4a6c
iasonasxrist <iasonasxrist@gmail.com>
2024-04-29 14:07:35 +0300

    Test

---
+++ CustomerManagement.BLTest/CustomerTest.cs
@@ -0,0 +1,59 @@
+using System.Net.Mail;
+using CustomerManagement.BL;
+
+public class CustomerTest
+{
+    [Fact]
+    public void FullNameMatching()
+    {
+
+        Customer customer = new Customer(1)
+        {
+            FirstName = "John",
+            LastName = "Doe"
+        };
+
+        // Act
+        string fullName = customer.FullName;
+
+        // Assert
+        Assert.Equal("John, Doe", fullName);
+    }
+    [Fact]
+    public void FullNameIsEmpty()
+    {
+
+        Customer customer = new Customer(1)
+        {
+            FirstName = "John"
+        };
+        string expected = "John";
+
+        // Act
+        string actual = customer.FullName;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void ValidateValid()
+    {
+
+        Customer customer = new Customer(1)
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            EmailAddress = "ias@gmail.com"
+        };
+
+        var expected = true;
+
+        // Act
+        var actual = customer.Validate();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+}
