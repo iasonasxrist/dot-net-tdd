4eb0a589ce599b0ee7a29b5aba36ea5c4785d8ac
iasonasxrist <iasonasxrist@gmail.com>
2024-04-30 17:34:10 +0300

    Logging

---
+++ CustomerManagement.BL/Helper/LoggingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class LoggingService
+{
+	public static void WriteToFile(List<ILoggable> itemsToLog)
+	{
+
+		foreach (var item in itemsToLog)
+		{
+			Console.WriteLine(item.Log());
+		}
+	}
+	
+}
+
